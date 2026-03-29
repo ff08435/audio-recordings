2a9e0afc578166b295f50926965c234ba794df02
Fatima Faisal <122521082+ff08435@users.noreply.github.com>
2026-03-29 23:26:26 +0500

    Update import path for supabase in FeedbackModal

--- src/Components/FeedbackModal.jsx
+++ src/Components/FeedbackModal.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../utils/supabase";
 
 export default function FeedbackModal({
   open,
